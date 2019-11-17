c600018fdfd9ef09904478b3aa5ce2ad3ef98e7d
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-11-17 23:45:50 +0700

    Updates

---
+++ precache-manifest.3bcc1a1a68de2ef491d976303f52b290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac826072356fb91416ec17c60486658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ead151ad3f5117438dc5",
+    "url": "/static/css/main.de689d65.chunk.css"
+  },
+  {
+    "revision": "9d719db869b92587d281",
+    "url": "/static/js/2.b30b71e3.chunk.js"
+  },
+  {
+    "revision": "ead151ad3f5117438dc5",
+    "url": "/static/js/main.b6e5792c.chunk.js"
+  },
+  {
+    "revision": "b241e8725cd2fe0c1196",
+    "url": "/static/js/runtime-main.bb8d79b8.js"
+  },
+  {
+    "revision": "d014957d144af31e420b11ca68601a75",
+    "url": "/static/media/wrong.d014957d.svg"
+  }
+]);
