c73b529d77939d06aec65db7f5b109570c7db77a
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-05-20 12:26:08 +0700

    fix: integration test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,73 +1,65 @@
-import "@testing-library/jest-dom/extend-expect";
-import { render, fireEvent, cleanup, act, wait } from "@testing-library/react";
-import React from "react";
-import axios from "axios";
-import App from "./App";
-import { AppProvider } from "./store/AppContext";
-import { SUCCESS_RESPONSE, ERROR_RESPONSE } from "./__mocks__/response";
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, cleanup, act, wait } from '@testing-library/react'
+import React from 'react'
+import axios from 'axios'
+import App from './App'
+import { AppProvider } from './store/AppContext'
+import { SUCCESS_RESPONSE, ERROR_RESPONSE } from './__mocks__/response'
 
-jest.mock("axios", () => ({
-  get: jest.fn(),
-}));
+jest.mock('axios', () => ({
+  get: jest.fn()
+}))
 
-global.open = jest.fn();
+global.open = jest.fn()
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-it("renders without crashing", async () => {
+it('renders without crashing', async () => {
   axios.get = jest
     .fn()
     .mockResolvedValueOnce({ data: SUCCESS_RESPONSE })
     .mockResolvedValueOnce({ data: ERROR_RESPONSE })
     .mockRejectedValueOnce({ data: ERROR_RESPONSE })
-    .mockResolvedValueOnce({ data: SUCCESS_RESPONSE });
+    .mockResolvedValueOnce({ data: SUCCESS_RESPONSE })
 
-  const {
-    getByText,
-    findByTestId,
-    getByTestId,
-    queryAllByTestId,
-    findAllByTestId,
-  } = render(
+  const { getByText, findByTestId, getByTestId, queryAllByTestId, findAllByTestId } = render(
     <AppProvider>
       <App />
-    </AppProvider>,
-  );
+    </AppProvider>
+  )
 
   // SHOULD SHOW LOADING COMPONENT WHEN FETCHING
-  const loadingComponent = queryAllByTestId("movie-loading");
-  expect(loadingComponent[0]).not.toBeInTheDocument();
+  const loadingComponent = queryAllByTestId('movie-loading')
+  expect(loadingComponent[0]).toBeInTheDocument()
 
   // WHEN FIRST INIT SHOULD FETCH MOVIE DATA
-  const listMovie = await findAllByTestId("movie-list");
-  expect(listMovie.length).toEqual(SUCCESS_RESPONSE.Search.length);
+  const listMovie = await findAllByTestId('movie-list')
+  expect(listMovie.length).toEqual(SUCCESS_RESPONSE.Search.length)
 
-  const searchInput = getByTestId("movie-search-input");
-  const searchButton = getByTestId("movie-search-button");
+  const searchInput = getByTestId('movie-search-input')
+  const searchButton = getByTestId('movie-search-button')
 
-  fireEvent.change(searchInput, { target: { value: "mamam" } });
-  fireEvent.click(searchButton);
-  await wait();
+  fireEvent.change(searchInput, { target: { value: 'mamam' } })
+  fireEvent.click(searchButton)
+  await wait()
 
-  const movieError = await findByTestId("movie-error");
-  expect(movieError).toBeInTheDocument();
-  expect(getByTestId("movie-search-input").value).toEqual("");
+  const movieError = await findByTestId('movie-error')
+  expect(movieError).toBeInTheDocument()
+  expect(getByTestId('movie-search-input').value).toEqual('')
 
-  fireEvent.click(searchButton);
-  await wait();
+  fireEvent.click(searchButton)
+  await wait()
 
-  expect(getByText(/Something went wrong/)).toBeInTheDocument();
+  expect(getByText(/Something went wrong/)).toBeInTheDocument()
 
-  fireEvent.change(searchInput, { target: { value: "spiderman" } });
+  fireEvent.change(searchInput, { target: { value: 'spiderman' } })
   act(() => {
-    fireEvent.click(searchButton);
-  });
-  expect(axios.get).toHaveBeenLastCalledWith(
-    "https://www.omdbapi.com/?s=spiderman&apikey=4a3b711b",
-  );
+    fireEvent.click(searchButton)
+  })
+  expect(axios.get).toHaveBeenLastCalledWith('https://www.omdbapi.com/?s=spiderman&apikey=4a3b711b')
 
-  const newListMovie = await findAllByTestId("movie-list");
-  expect(newListMovie.length).toEqual(SUCCESS_RESPONSE.Search.length);
-  fireEvent.click(newListMovie[0]);
-  expect(global.open).toBeCalled();
-});
+  const newListMovie = await findAllByTestId('movie-list')
+  expect(newListMovie.length).toEqual(SUCCESS_RESPONSE.Search.length)
+  fireEvent.click(newListMovie[0])
+  expect(global.open).toBeCalled()
+})
